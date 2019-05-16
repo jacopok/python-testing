0754976dce77bca63c0abda78f1c608b98cab109
jacopo <jacopok@gmail.com>
2019-05-16 15:33:03 +0200

    Got the lomb-scargle periodogram working with loosely sampled data

--- lomb-scargle/lomb-scargle.py
+++ lomb-scargle/lomb-scargle.py
@@ -1,16 +1,40 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from scipy.signal import lombscargle
+from scipy.stats import linregress
+
 plt.style.use('bmh')
 
 
 data = pd.read_csv('data.csv', delimiter='  ')
 
 x = data['x'].values
-y = data['y'].values
+y_test = data['y'].values
+
+slope, intercept, r, p, err = linregress(x, y_test)
+y = y_test - slope * x - intercept
+
+n = len(x)
+nfreq = 10*n
+
+dxmin = np.diff(x).min()
+duration = x.ptp()
+freqs = np.linspace(1/duration, n/duration, nfreq)
+periodogram = lombscargle(x, y, freqs)
+
+kmax = periodogram.argmax()
+print("%8.3f" % (freqs[kmax],))
 
 plt.figure(1)
+plt.plot(freqs, np.sqrt(4*periodogram/(nfreq)))
+plt.xlabel('Frequency (rad/s)')
+plt.axvline(freqs[kmax], color='r', alpha=0.25)
+plt.show()
+
+plt.figure(2)
 plt.plot(x, y, 'b')
+plt.plot(x, np.sin(x*freqs[kmax])*np.max(y), 'r')
 plt.xlabel('x')
 plt.ylabel('y')
 
