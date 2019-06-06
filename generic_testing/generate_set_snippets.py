fd38c7f391533bd12d47f14edd91bf61e228f4f1
jacopo <jacopok@gmail.com>
2019-06-06 18:16:31 +0200

    added some more commands

--- generic_testing/generate_set_snippets.py
+++ generic_testing/generate_set_snippets.py
@@ -1,6 +1,9 @@
-qm_commands0 = {'geq', 'leq', 'forall', 'exists', 'dag', 'cdot', 'iff', }
+qm_commands0 = {'geq', 'leq', 'forall', 'exists', 'dag', 'cdot', 'iff', 'hbar'}
 qm_commands1 = {'ket', 'bra', 'dyad', 'expval'}
-qm_commands2 = {'braket', 'ketbra'}
+qm_commands2 = {'braket', 'ketbra', 'pdv', 'dv', 'fdv'}
+
+new0 = {'otimes', 'divisionsymbol', 'sim', 'leftrightarrow', 'equiv', 'log'}
+new1 = {'abs', 'sqrt', 'P', 'sin', 'cos', 'tan'}
 
 def no_arguments(commands):
     for c in commands:
@@ -23,9 +26,12 @@ def two_argument(commands):
         print(f'  \'body\': \'\\\\\\\\{c}{{$1}}{{$2}} $3\'')
         print()
 
-no_arguments(qm_commands0)
-one_argument(qm_commands1)
-two_argument(qm_commands2)
+#no_arguments(qm_commands0)
+# one_argument(qm_commands1)
+# two_argument(qm_commands2)
+
+no_arguments(new0)
+one_argument(new1)
 
 """
   'Modulus':
