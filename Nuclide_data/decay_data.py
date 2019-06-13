fc13649e666d438779381c19c423e61ba0d08391
jacopo <jacopok@gmail.com>
2019-06-13 23:14:51 +0200

    Started decay data analysis

--- Nuclide_data/decay_data.py
+++ Nuclide_data/decay_data.py
@@ -1,12 +1,16 @@
 import pandas as pd
 import numpy as np
 
-data = pd.read_csv('livechart.csv', index_col=False)
+data = pd.read_csv('binding_energies.csv', index_col=False)
 
-intensities = data.iloc[:,2].values
-iarray = []
-for x in intensities:
-    if x:
-        iarray.append(x.strip())
-iarray = list(filter(None, iarray))
-intensities = np.array(iarray, dtype=np.float)
+# intensities = data.iloc[:,2].values
+# iarray = []
+# for x in intensities:
+#     if x:
+#         iarray.append(x.strip())
+# iarray = list(filter(None, iarray))
+# intensities = np.array(iarray, dtype=np.float)
+
+energies = data['Binding/A'].values
+
+even_energies = energies[::2]
