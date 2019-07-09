6b5578e2dd19740a3b16b5f4296add8c5fbbf20d
jacopo <jacopok@gmail.com>
2019-07-09 17:55:48 +0200

    Added some markdown notebooks

--- quantum_sympy/gates.py
+++ quantum_sympy/gates.py
@@ -1,3 +1,5 @@
+# %%
+
 from sympy.physics.quantum import *
 from sympy.physics.quantum.qubit import *
 from sympy.physics.paulialgebra import Pauli
@@ -8,15 +10,24 @@ plt.style.use('seaborn')
 
 from sympy import *
 
+# %%
+
 dim = 2**2
 M = np.reshape(np.ones(dim**2), (dim, dim))
 id = np.identity(dim)
 D = 1/dim * (M - 2 * id)
 
-for x in range(dim):
-    vec = np.ones(dim)
-    vec[x] -=2
-    print(D@vec)
+# %% markdown
+
+We show the form of the operators used in the Grover algorithm
+in the $n=2$ case.
+
+# %%
+
+# for x in range(dim):
+#     vec = np.ones(dim)
+#     vec[x] -=2
+#     print(D@vec)
 H = 1/np.sqrt(2)* np.array([[1,1], [1,-1]])
 H2 = np.kron(H, H)
 sigma_x = np.array([[0,1], [1,0]])
@@ -24,9 +35,17 @@ sigma_x2 = np.kron(sigma_x, sigma_x)
 CNOT = np.array([[1,0,0,0],[0,1,0,0],[0,0,0,1],[0,0,1,0]])
 idH = np.kron(np.identity(2), H)
 Dprime = sigma_x2 @ idH @ CNOT @ idH @ sigma_x2
-print('Dprime = ', sigma_x2 @ idH @ CNOT @ idH @ sigma_x2)
+print('Dprime = ', Dprime)
 D = H2 @ sigma_x2 @ idH @ CNOT @ idH @ sigma_x2 @ H2
+print('D = ', D)
 
-Dprime
-D @ np.array([1,1,1,-1])
-H@H
+# %%
+
+sigma = [
+Matrix([[0,1],[1,0]]),
+Matrix([[0,I],[-I,0]]),
+Matrix([[1,0],[-1,0]])
+]
+
+for s in sigma:
+    print(H * s * H)
