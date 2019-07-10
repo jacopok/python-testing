a94f9be26c921af4484897396f8894175b2241a7
jacopo <jacopok@gmail.com>
2019-07-10 17:46:24 +0200

    plotted the integrated function

--- saved_functions/sympy_integration.py
+++ saved_functions/sympy_integration.py
@@ -1,5 +1,6 @@
 # %%
 
+import numpy as np
 import sympy as sp
 sp.init_printing()
 
@@ -8,5 +9,10 @@ x = sp.symbols('x')
 f = sp.sin(x)**2 / x**2
 
 # %%
-import sympy as sp
+
 sp.integrate(f, (x, -sp.oo, +sp.oo))
+
+# %%
+
+%matplotlib inline
+sp.plot(f)
