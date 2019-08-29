2e901ddd75700a77ad990646c9335721714047d9
jacopo <jacopok@gmail.com>
2019-08-29 19:41:54 +0200

    Added legendre polynomials roots

--- spherical_accretion/parameter_calculations.py
+++ spherical_accretion/parameter_calculations.py
@@ -11,3 +11,17 @@ gamma = 1/sp.sqrt(1 - v**2)
 x = sp.sqrt(v**2 + 1/gamma**2)
 
 sp.simplify(x)
+
+#%%
+
+from mpmath import legendre
+import numpy as np
+import matplotlib.pyplot as plt
+plt.style.use('seaborn')
+from mpmath import *
+
+f = lambda x : legendre(2,x)
+
+plot(f)
+
+nprint(polyroots(taylor(f, 0, 2)[::-1]))
