78ef0d9c741bf0cbdd01ad94efc09b0dbcecdb8f
jacopo <jacopok@gmail.com>
2019-10-31 19:57:36 +0100

    Exercises

--- numerical_methods/contours/first_chapter_contours.py
+++ numerical_methods/contours/first_chapter_contours.py
@@ -9,21 +9,34 @@ tmerg_filename = "tmerg_bin.dat"
 chirpmass_filename = "chirpmass_bin.dat"
 
 with open(tot_filename) as tot_file:
-    tot_data = [np.array(i.strip().split()) for i in tot_file.readlines()]
+    tot_data = [np.array(i.strip().split(), dtype=np.float) for i in tot_file.readlines()]
 with open(tmerg_filename) as tmerg_file:
     tmerg_data = [i.strip() for i in tmerg_file.readlines() if i[0]!='#']
 with open(chirpmass_filename) as chirpmass_file:
     chirpmass_data = [i.strip() for i in chirpmass_file.readlines() if i[0]!='#']
 
-tot_data = np.array(tot_data)
-tmerg_data = np.array(tmerg_data)
-chirpmass_data = np.array(chirpmass_data)
+tot_data = np.array(tot_data, dtype=np.float)
+tmerg_data = np.array(tmerg_data, dtype=np.float)
+chirpmass_data = np.array(chirpmass_data, dtype=np.float)
 
 # %%
 
 import matplotlib.cm as cm
+import matplotlib.ticker as ticker
 
-plt.ylim((0, 50))
-plt.contour(tmerg_data, chirpmass_data, tot_data, cmap=cm.viridis, Nchunk = 10)
+plt.ylim((0, 40))
+
+ctf = plt.contourf(tmerg_data, chirpmass_data, tot_data, cmap=cm.plasma, locator=ticker.LogLocator())
+ct = plt.contour(tmerg_data, chirpmass_data, tot_data, locator=ticker.LogLocator())
+
+def fmt(x):
+    return(f'{x:.0e}')
+
+# ct.levels = [fmt(i) for i in ct.levels]
+
+cb = plt.colorbar(ctf)
+cb.set_ticks([fmt(x) for x in cb.get_ticks()])
+plt.clabel(ct, ct.levels)
+
 
 # %%
