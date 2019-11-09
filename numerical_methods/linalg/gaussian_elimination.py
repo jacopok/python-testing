898d9b1ab7aa02df0bd4b7264c4f98f01b7fd1bb
jacopo <jacopok@gmail.com>
2019-11-09 13:06:20 +0100

    Tried stuff

--- numerical_methods/linalg/gaussian_elimination.py
+++ numerical_methods/linalg/gaussian_elimination.py
@@ -4,8 +4,9 @@ from time import sleep
 
 def test_data(n, pathological=False):
     from numpy.random import rand
-    A = rand(n, n)
-    b = rand(n)
+    A = np.array(rand(n, n)*10, dtype=int)
+    b = np.array(rand(n)*10, dtype=int)
+    # b = rand(n)
     if(pathological):
         for i in range(n):
             A[i,i] = 0
@@ -22,37 +23,39 @@ def gaussian_elimination(A, b):
         raise TypeError("column number of A must equal dimension of b")
 
     n_rows, n_cols = A_shape
-    A = np.array(A)
-    b = np.array(b)
-    # print(solve(A, b))
+    A = np.array(A, dtype=np.float64)
+    b = np.array(b, dtype=np.float64)
 
     scale = np.average([np.average(np.abs(A.flatten())), np.average(np.abs(b))])
 
     def is_zero(x, scale=scale, threshold=1e-10):
+        """
+        Check whether a number is significantly 
+        less than "scale", the average absolute value of the numbers in the tensors at hand. 
+        This is done instead of a constant threshold to prevent issues with matrices of very small or large numbers.
+        """
+
         return(np.abs(x)<threshold*scale)
 
     if (is_zero(det(A))):
         raise TypeError("Zero determinant")
-    
-    def roll(A, start=0):
-        for j in range(start, len(A) - 1):
-            A[j], A[j + 1] = A[j + 1], A[j]
-        return (A)
-    
+        
     print("Solutions", solve(A, b))
+    print(A, b)
     
     i=0
     while (i<n_cols):
         pivot = A[i, i]
-        # print("Det:", det(A))
-        # print(A)
         if (pivot == 0):
-            A[i:,:] = np.roll(A[i:,:], 1, axis=0)
-            b[i:] = np.roll(b[i:], 1, axis=0)
-            # A = roll(A)
-            sleep(1)
+            # print("Rolling", A, b)
+            for x in (A, b):
+                x[i:] = np.roll(x[i:],1,axis=0)
+            # A[i:,:] = np.roll(A[i:], 1, axis=0)
+            # b[i:] = np.roll(b[i:], 1, axis=0)
+            # print("Result", A, b)
             continue
-        A[i,:] /= pivot
+        A[i,:] = A[i,:] /  pivot
+        b[i] = b[i] / pivot
         for j in range(i+1, n_rows):
             A[j,:] -= A[i,:] * A[j, i]
             b[j] -= b[i] * A[j, i]
