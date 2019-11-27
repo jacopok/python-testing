579c81aa75c6f52cd7d3a5e7b3804b5610254fce
jacopo <jacopok@gmail.com>
2019-11-27 13:36:28 +0100

    New exercise
    Former-commit-id: 56a114c6e4b6e6a273bf9ffe52389f9ec700bf13

---
+++ advanced_astrophysics/isothermal_stellar_wind_energies.py
@@ -0,0 +1,41 @@
+# %%
+
+from scipy.constants import *
+from math import sqrt
+
+T = 1.5e6 # K
+M_sun = 1.98847e30 # kg
+R_sun = 696e6 # m
+year = 365 * 24 * 3600  # s 
+Mdot = 2e-14 * M_sun / year
+r0 = 1.003 * R_sun
+cgs_to_mks_density = * 1e-3 * (1e-2)**(-3)
+rho0 = 1e-14 * cgs_to_mks_density  # kg/m**3
+mu = .62
+
+# %%
+
+v0 = Mdot / (4* pi * r0**2 * rho0)
+
+E_kin0 = .5 * v0**2 # J/kg
+E_grav0 = - G * M_sun / r0   
+E_chem0 = 5/2* R*T/mu
+
+# %%
+
+a = sqrt(R * T / mu)
+E_kin_crit = .5 * a ** 2
+E_grav_crit = - 2 * a**2
+
+# %% [markdown]
+# New exercise
+
+# %%
+
+Teff = 3200  # K
+R_star = 30 * R_sun
+L_sun = 3.828e26 # W
+L_star = 85 * L_sun
+M_star = 6 * M_sun
+T_star = 1e6  # K
+rho0_star = 1e-13 * cgs_to_mks_density
