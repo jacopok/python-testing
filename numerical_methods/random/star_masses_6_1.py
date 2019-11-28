21935097dda970b6e86fcbb2f8edf36cde147022
jacopo <jacopok@gmail.com>
2019-11-28 15:31:11 +0100

    exercise

--- numerical_methods/random/star_masses_6_1.py
+++ numerical_methods/random/star_masses_6_1.py
@@ -4,10 +4,9 @@ from astropy.visualization import astropy_mpl_style
 plt.style.use(astropy_mpl_style)
 from scipy.optimize import curve_fit
 
-alpha = 2.3
 N=int(1e7)
 
-def m_from_x(x, alpha=2.3, m_min=0.1, m_max=150):
+def m_from_x(x, alpha=3.3, m_min=0.1, m_max=150):
     N = 1 / (1 - alpha) * (m_max ** (1 - alpha) - m_min ** (1 - alpha))
     m_oneminusalpha = (m_min ** (1 - alpha) + (1 - alpha) * N * x)
     return (m_oneminusalpha ** (1 / (1 - alpha)))
@@ -15,12 +14,14 @@ def m_from_x(x, alpha=2.3, m_min=0.1, m_max=150):
 uniform_deviates = np.random.uniform(size=N)
 
 ms = m_from_x(uniform_deviates)
+log_ms = np.log(ms)
 
-vals, bins, _ = plt.hist(ms, bins=100, log=True)
+vals, bins, _ = plt.hist(log_ms, bins=100)
+vals = vals+0.1
 
-bins = np.sqrt((bins[:-1]*bins[1:]))
-f = lambda x, alpha, C: C * x**(-alpha)
-p, pcov = curve_fit(f, bins, vals, p0=[2, 1e6], sigma=np.sqrt(vals)/np.sqrt(max(vals)))
+bins = (bins[:-1]+bins[1:])/2
+f = lambda x, alpha, C : C * np.exp(-alpha*x)
+p, pcov = curve_fit(f, bins, vals, p0=[2, N/10], sigma=np.sqrt(vals)/np.sqrt(max(vals)))
 
 plt.plot(bins, vals)
 plt.plot(bins, f(bins, *p))
