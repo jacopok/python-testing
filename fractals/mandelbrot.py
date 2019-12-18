9d60d7646b8ea4df684597eff5729ad72f506a28
jacopo <jacopok@gmail.com>
2019-12-18 10:49:20 +0100

    Created high res rendering

--- fractals/mandelbrot.py
+++ fractals/mandelbrot.py
@@ -5,12 +5,12 @@ from astropy.visualization import astropy_mpl_style
 plt.style.use(astropy_mpl_style)
 from matplotlib import cm
 
-ITMAX = int(2e11)
+ITMAX = int(2e6)
 
 def iterate(z, c):
     return(z**2 + c)
 
-def convergence(c, eps=1e-6):
+def convergence_mandelbrot(c, eps=1e-6):
 
     iterations = 0
     z = 0
@@ -23,21 +23,21 @@ def convergence(c, eps=1e-6):
         iterations += 1
 
 def generate_set(*args, N=int(1e3)):
-    x, y, r = args
-    xs = np.linspace(x-r, x+r, num=N)
-    ys = np.linspace(y-r, y+r, num=N)
+    x1, x2, y1, y2 = args
+    xs = np.linspace(x1, x2, num=N)
+    ys = np.linspace(y1, y2, num=N)
 
     cs = np.zeros((N, N))
 
     for i, x in tqdm(enumerate(xs)):
         for j, y in enumerate(ys):
             c = complex(x, y)
-            cs[j, i] = convergence(c)
+            cs[j, i] = convergence_mandelbrot(c)
     
     return(xs, ys, cs)
 
 def plot(xs, ys, cs):
     X, Y = np.meshgrid(xs, ys)
     plt.close()
-    colors = plt.contourf(X, Y, cs, cmap = cm.plasma)
+    colors = plt.contourf(X, Y, cs, cmap = plt.get_cmap('plasma'))
     plt.colorbar(colors)
