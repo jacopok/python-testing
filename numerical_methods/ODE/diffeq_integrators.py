1e4f8c1a8153d746779c426cc0a21a973b2622ea
jacopo <jacopok@gmail.com>
2019-12-18 16:30:23 +0100

    Refactored code so now it is much more general

--- numerical_methods/ODE/diffeq_integrators.py
+++ numerical_methods/ODE/diffeq_integrators.py
@@ -12,18 +12,32 @@ rc('text.latex', preamble=r'''\usepackage{amsmath}
 
 hdefault = .4
 
+# def initialize(t0, tmax, x0, h):
+#     ts = np.arange(t0, tmax, h)
+#     xs = np.zeros((len(ts), len(x0)))
+#     xs[0] = x0
+#     return(ts, xs)
+
 def initialize(t0, tmax, x0, h):
     ts = np.arange(t0, tmax, h)
-    xs = np.zeros((len(ts), len(x0)))
+    xs = np.zeros(((len(ts),) + np.shape(x0)))
     xs[0] = x0
     return(ts, xs)
+
+# def euler(f, t0, tmax, x0, h=hdefault):
+#     ts, xs = initialize(t0, tmax, x0, h)
+
+#     for i, t in enumerate(ts[:-1]):
+#         x = xs[i]
+#         xs[i+1] = x + h * f(*x, t)
+#     return (ts, xs)
 
 def euler(f, t0, tmax, x0, h=hdefault):
     ts, xs = initialize(t0, tmax, x0, h)
 
     for i, t in enumerate(ts[:-1]):
         x = xs[i]
-        xs[i+1] = x + h * f(*x, t)
+        xs[i+1] = x + h * f(x, t)
     return (ts, xs)
 
 def midpoint(f, t0, tmax, x0, h=hdefault):
@@ -31,9 +45,9 @@ def midpoint(f, t0, tmax, x0, h=hdefault):
 
     for i, t in enumerate(ts[:-1]):
         x = xs[i]
-        xhalf = x + h / 2. * f(*x, t)
+        xhalf = x + h / 2. * f(x, t)
         thalf = t + h / 2.
-        xs[i+1] = x + h * f(*xhalf, thalf)
+        xs[i+1] = x + h * f(xhalf, thalf)
     return (ts, xs)
 
 def fourth_order(f, t0, tmax, x0, h=hdefault):
@@ -43,29 +57,44 @@ def fourth_order(f, t0, tmax, x0, h=hdefault):
         x = xs[i]
         thalf = t + h / 2.
         tnew = t + h
-        k1 = h / 2. * f(*x, t)
-        k2 = h / 2. * f(*(x + k1), thalf)
-        k3 = h * f(*(x + k2), thalf)
-        k4 = h * f(*(x + k3), tnew)
+        k1 = h / 2. * f(x, t)
+        k2 = h / 2. * f((x + k1), thalf)
+        k3 = h * f(x + k2, thalf)
+        k4 = h * f(x + k3, tnew)
         xs[i+1] = x + (2.*k1 + 4.*k2 + 2.*k3 + k4)/6.
     return (ts, xs)
 
-# def leapfrog_KDK(f, t0, tmax, x0, h=hdefault):
-#     ts, xs = initialize(t0, tmax, x0, h)
+def leapfrog_KDK(G, t0, tmax, x0, v0, h=hdefault):
+    """
+    To be used only with second order problems, 
+    instead of a first order equation it takes
+    as input the second order one, G, which should be 
+    x'' = G(x, t)
+    """
 
-#     for i, t in enumerate(ts[:-1]):
+    ts, xs = initialize(t0, tmax, x0, h)
+    _, vs = initialize(t0, tmax, v0, h)
+    a_s = vs
+    a_s[0] = G(xs[0])
 
+    for i, t in enumerate(ts[:-1]):
+        x = xs[i]
+        v = vs[i]
+        a = a_s[i]
+        xs[i + 1] = x + h * v + h * h * a / 2.
+        a_s[i + 1] = G(x, t)
+        vs[i + 1] = v + h / 2. * (a + a_s[i + 1])
+    return(ts, np.array([xs, vs]))
 
 if __name__ == "__main__":
     ftest = lambda x, t: - x ** 3 + np.sin(t)
-    params = (0, 100, [0])
+    params = (0, 100, 0)
 
     t, x_euler = euler(ftest, *params)
     t, x_midpoint = midpoint(ftest, *params)
     t, x_fourth_order = fourth_order(ftest, *params)
     tc, x_correct = fourth_order(ftest, *params, h=hdefault / 100)
     x_c = x_correct[::100]
-    # Need to start from there because all integrators with large timesteps diverge from the correct path initially and pick up a phase of the order h
     x_e = x_euler - x_c
     x_m = x_midpoint - x_c
     x_f = x_fourth_order - x_c
