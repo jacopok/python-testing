e7231a09698e75bcef4248147eb7ae09f598fb80
jacopo <jacopok@gmail.com>
2019-12-20 00:50:50 +0100

    small changes, added new attractor

--- numerical_methods/ODE/attractor.py
+++ numerical_methods/ODE/attractor.py
@@ -22,13 +22,25 @@ def general_lorenz(x, t, sigma, rho, beta):
     y[2] = x[0] * x[1] - beta * x[2]
     return (y)
     
-params = {'sigma': 10., 'beta': 8. / 3., 'rho': 29.}
-lorenz = partial(general_lorenz, **params)
+params_lorenz = {'sigma': 10., 'beta': 8. / 3., 'rho': 29.}
+lorenz = partial(general_lorenz, **params_lorenz)
 
-ts, xs = fourth_order(lorenz, 0, 40, [1., 1., 1.], h=1e-4)
+def general_rossler(x, t, A, B, C):
+    y = np.zeros(3)
+    y[0] = -(x[1] + x[2])
+    y[1] = x[0] + A * x[1]
+    y[2] = B + x[0] * x[2] - C * x[2]
+    return(y)
+
+
+params_rossler={'A': .2, 'B': .2, 'C': 5.7}
+rossler = partial(general_rossler, **params_rossler)
 
 def plot(xs):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    ax.plot(xs[:, 0], xs[:, 1], xs[:, 2], linewidth=1)
+    ax.plot(xs[:, 0], xs[:, 1], xs[:, 2], linewidth=.5)
     plt.show()
+
+# ts, xs = fourth_order(lorenz, 0, 40, [1., 1., 1.], h=1e-4)
+ts, xs = fourth_order(rossler, 0, 400, [1, 1, 1], h=1e-3)
