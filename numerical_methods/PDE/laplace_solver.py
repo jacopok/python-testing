f91432d9a19141686304c9da2d16be3c9d32e77d
jacopo <jacopok@gmail.com>
2019-12-26 23:37:15 +0100

    Wrote Laplace Solver which uses GS

--- numerical_methods/PDE/laplace_solver.py
+++ numerical_methods/PDE/laplace_solver.py
@@ -10,18 +10,42 @@ rc('text.latex', preamble=r'''\usepackage{amsmath}
           \usepackage{siunitx}
           ''')
 
-N = 40 # x number of points
-M = 50 # y number of points
+N = 101 # x number of points
+M = 101 # y number of points
 dimensions = (N, M)
 
-def two_to_one(i, j, N=N):
-    return (N * j + i)
+field = np.zeros(dimensions)
+field[0,:] = 1.
 
-def one_to_two(I, N=N):
-    return((int(I%N), int(I/N)))
+def sum_neighbours(i, j, matrix):
+    neighbours = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
+    sum = 0.
+    for ind in neighbours:
+        sum += matrix[ind]
+    return(sum)
 
-def get_neighbours(I, N=N):
-    i, j = one_to_two(I, N)
-    return ([(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)])
+def iteration_step(field,
+        relax=1,
+        charge_density=None,
+        a=None):
+    inner_field = field[1:-1,1:-1]
+    if not charge_density:
+        # Rescaled Charge Density: 
+        # should be rho / a^2
+        rcd = np.zeros_like(inner_field)
     
-# def create_matrix():
+
+    error = 0.
+    for i, row in enumerate(inner_field):
+        for j, phi in enumerate(row):
+            old_field = np.copy(phi)
+            inner_field[i, j] = relax * (\
+            sum_neighbours(i+1, j+1, field) / 4.
+            + rcd[i, j])\
+            + (relax - 1) * phi
+            if(inner_field[i,j] > 0.):
+                error += np.abs((inner_field[i, j] - old_field) / inner_field[i,j])
+    n, m = np.shape(field)
+    relative_error = error / (n*m)
+
+    return (field, relative_error)
