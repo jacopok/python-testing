d0b4080416380a253f9f195dacca0250e2ba654e
jacopo <jacopok@gmail.com>
2019-12-27 00:53:45 +0100

    Updated laplace solver
    Former-commit-id: 408fb3d9f0610573f16fad4765920e47355be45e

--- numerical_methods/PDE/laplace_solver.py
+++ numerical_methods/PDE/laplace_solver.py
@@ -9,13 +9,15 @@ rc('text.latex', preamble=r'''\usepackage{amsmath}
           \usepackage{physics}
           \usepackage{siunitx}
           ''')
+from math import e as neper
+from time import time
 
 N = 101 # x number of points
 M = 101 # y number of points
 dimensions = (N, M)
 
-field = np.zeros(dimensions)
-field[0,:] = 1.
+test_field = np.zeros(dimensions)
+test_field[0,:] = 1.
 
 def sum_neighbours(i, j, matrix):
     neighbours = [(i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1)]
@@ -33,19 +35,48 @@ def iteration_step(field,
         # Rescaled Charge Density: 
         # should be rho / a^2
         rcd = np.zeros_like(inner_field)
-    
-
     error = 0.
     for i, row in enumerate(inner_field):
         for j, phi in enumerate(row):
-            old_field = np.copy(phi)
-            inner_field[i, j] = relax * (\
-            sum_neighbours(i+1, j+1, field) / 4.
-            + rcd[i, j])\
-            + (relax - 1) * phi
-            if(inner_field[i,j] > 0.):
-                error += np.abs((inner_field[i, j] - old_field) / inner_field[i,j])
+            new_field = sum_neighbours(i+1, j+1, field) / 4. \
+            + rcd[i, j]
+            if(new_field > 0.):
+                error += np.abs((new_field - phi) / new_field)
+            inner_field[i, j] = relax * new_field \
+                + (1 - relax) * phi
     n, m = np.shape(field)
     relative_error = error / (n*m)
 
     return (field, relative_error)
+
+def test_iteration(f, iter, tol, **kwargs):
+    field = np.copy(f)
+    field, e0 = iter(field, **kwargs)
+    errs = [e0]
+
+    error_efolds = np.log(e0 / tol)
+    print(f"{int(error_efolds)+1} error e-folds to go")
+    current_efold = e0 / neper
+    old_time = time()
+    efold_counter = 1
+
+    while errs[-1] > tol:
+        field, e = iter(field, **kwargs)
+        errs.append(e)
+
+        if (e < current_efold):
+            new_time = time()
+            print(f"Finished efold {efold_counter} in {new_time - old_time :.2f} seconds")
+            old_time = new_time
+            efold_counter += 1
+            current_efold /= neper
+
+        if (e > 10 * e0):
+            print("algorithm diverged")
+            return (None)
+    
+    new_time = time()
+    print(f"Finished what remained of efold {efold_counter} in {new_time - old_time :.2f} seconds")
+
+    print(f"Finished in {len(errs)} steps")
+    return(field, errs)
