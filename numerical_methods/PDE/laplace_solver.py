7ae0f27a07686853cf0851c02b43cd4503ae64b4
jacopo <jacopok@gmail.com>
2019-12-28 13:22:35 +0100

    Laplace updates

--- numerical_methods/PDE/laplace_solver.py
+++ numerical_methods/PDE/laplace_solver.py
@@ -5,10 +5,7 @@ plt.style.use(astropy_mpl_style)
 from matplotlib import rc
 # rc('font',**{'family':'serif','serif':['Palatino']})
 rc('text', usetex=True)
-rc('text.latex', preamble=r'''\usepackage{amsmath}
-          \usepackage{physics}
-          \usepackage{siunitx}
-          ''')
+
 from math import e as neper
 from time import time
 
@@ -33,10 +30,11 @@ def iteration_step(field,
     inner_field = field[1:-1,1:-1]
     if charge_density is None:
         # Rescaled Charge Density: 
-        # should be rho / a^2
+        # should be rho * a^2 / 4. / epsilon_0
+        # I omit the dielectric permeability factor for simplicity
         rcd = np.zeros_like(inner_field)
     else:
-        rcd = charge_density / a**2
+        rcd = charge_density * a**2 / 4.
     error = 0.
     for i, row in enumerate(inner_field):
         for j, phi in enumerate(row):
@@ -45,13 +43,19 @@ def iteration_step(field,
             if(new_field != 0.):
                 error += np.abs((new_field - phi) / new_field)
             inner_field[i, j] = relax * new_field \
-                - (1 - relax) * phi
-    n, m = np.shape(field)
+                + (1 - relax) * phi
+    n, m = np.shape(inner_field)
     relative_error = error / (n*m)
 
     return (field, relative_error)
 
 def test_iteration(f, iter, tol, **kwargs):
+    rel = 'relax'
+    if rel in kwargs:
+        print(f'Testing Laplace solver with relaxation {kwargs[rel]}')
+    else:
+        print('Testing Laplace solver without relaxation')
+
     field = np.copy(f)
     field, e0 = iter(field, **kwargs)
     errs = [e0]
@@ -81,4 +85,24 @@ def test_iteration(f, iter, tol, **kwargs):
     print(f"Finished what remained of efold {efold_counter} in {new_time - old_time :.2f} seconds")
 
     print(f"Finished in {len(errs)} steps")
-    return(field, errs)
+    return (field, errs)
+
+if __name__ == "__main__":
+    
+    f1, e1 = test_iteration(test_field, iteration_step, tol=1e-3)
+    f2, e2 = test_iteration(test_field, iteration_step, tol=1e-3, relax=1.9)
+
+    fig, axs = plt.subplots(2,2)
+
+    axs[0, 0].imshow(f1)
+    axs[0,0].set_title('Relaxation = 1.')
+    axs[0, 1].semilogy(e1)
+    axs[0,1].set_xlabel('Iteration number')
+    axs[0,1].set_ylabel('Relative error $\\epsilon$')
+
+    axs[1, 0].imshow(f2)
+    axs[1,0].set_title('Relaxation = 1.9')
+    axs[1, 1].semilogy(e2)
+    axs[1,1].set_xlabel('Iteration number')
+    axs[1,1].set_ylabel('Relative error $\\epsilon$')
+    plt.show()
