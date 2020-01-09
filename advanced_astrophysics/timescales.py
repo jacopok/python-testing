1b895c8d9799d94629d206dc9b17c2943c975f7b
jacopo <jacopok@gmail.com>
2020-01-09 20:29:31 +0100

    Updated timescales

--- advanced_astrophysics/timescales.py
+++ advanced_astrophysics/timescales.py
@@ -1,7 +1,7 @@
 import astropy.constants as ac 
 
-tau_th = (ac.GM_sun * ac.M_sun / ac.L_sun / ac.R_sun)
-tau_dyn = np.sqrt(ac.R_sun ** 3 / ac.GM_sun)
+tau_th = (ac.GM_sun * ac.M_sun / ac.L_sun / ac.R_sun) / 2
+tau_dyn = np.sqrt(ac.R_sun ** 3 / ac.GM_sun) 
 
 eps = 7e-3
 H_frac = 1e-1
