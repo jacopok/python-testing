473a608bae92dc9bbf1f4922734c827f66c746e8
jacopo <jacopok@gmail.com>
2020-01-15 17:53:07 +0100

    Updated 9th chapter exercises

--- numerical_methods/PDE-9/laplace_solver_9_1.py
+++ numerical_methods/PDE-9/laplace_solver_9_1.py
@@ -88,20 +88,7 @@ def test_iteration(f, iter, tol, **kwargs):
 
 if __name__ == "__main__":
     
-    f1, e1 = test_iteration(test_field, iteration_step, tol=1e-2)
-    f2, e2 = test_iteration(test_field, iteration_step, tol=1e-2, relax=1.9)
-
-    fig, axs = plt.subplots(2,2)
-
-    axs[0, 0].imshow(f1)
-    axs[0,0].set_title('Relaxation = 1.')
-    axs[0, 1].semilogy(e1)
-    axs[0,1].set_xlabel('Iteration number')
-    axs[0,1].set_ylabel('Relative error $\\epsilon$')
-
-    axs[1, 0].imshow(f2)
-    axs[1,0].set_title('Relaxation = 1.9')
-    axs[1, 1].semilogy(e2)
-    axs[1,1].set_xlabel('Iteration number')
-    axs[1,1].set_ylabel('Relative error $\\epsilon$')
+    f, e = test_iteration(test_field, iteration_step, tol=1e-5, relax=1.9)
+    plt.imshow(f)
     plt.show()
+    
