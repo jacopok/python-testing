d3927e6a0c02cab9d28ed0b9e696b593ad3e9d57
jacopo <jacopok@gmail.com>
2020-01-15 12:00:25 +0100

    Wrote exercise 3.1
    Former-commit-id: ce83ccb99c86445dd143711b425be21c6b253ead

--- numerical_methods/linalg-3/gaussian_elimination.py
+++ numerical_methods/linalg-3/gaussian_elimination.py
@@ -1,5 +1,9 @@
 import numpy as np
 from numpy.linalg import matrix_rank, solve
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+plt.style.use(astropy_mpl_style)
+from scipy.optimize import curve_fit
 
 def test_data(nrows, ncols=None, pathological=False, whole=True, diagonal_multiplier=1, positive_definite=False):
     from numpy.random import rand
@@ -23,49 +27,6 @@ def test_data(nrows, ncols=None, pathological=False, whole=True, diagonal_multip
     if (positive_definite):
         A = A @ A.T
     return(A, b)
-
-def gaussian_elimination(A, b):
-    A_shape = np.shape(A)
-    b_shape = np.shape(b)
-    if (len(A_shape) != 2):
-        raise TypeError("A must be a 2D matrix")
-    if (len(b_shape) != 1):
-        raise TypeError("b must be a 1D vector")
-    if (A_shape[0] != b_shape[0]):
-        raise TypeError("column number of A must equal dimension of b")
-
-    n_rows, n_cols = A_shape
-    A = np.array(A, dtype=np.float64)
-    b = np.array(b, dtype=np.float64)
-
-    if (matrix_rank(A) < min(n_rows, n_cols)):
-        raise TypeError("Rank too low")
-        
-    i=0
-    while (i<n_rows):
-        pivot = A[i, i]
-        if (pivot == 0):
-            for x in (A, b):
-                x[i:] = np.roll(x[i:],1,axis=0)
-            continue
-        A[i,:] = A[i,:] /  pivot
-        b[i] = b[i] / pivot
-        for j in range(i+1, n_rows):
-            a = A[j,i]
-            A[j,:] -= A[i,:] * a
-            b[j] -= b[i] * a
-        i += 1
-
-    # BACKSUBSTITUTION
-    # this works as long as the matrix is 
-    # upper triangular with ones on the diagonal
-    i = n_rows - 1
-    x = np.zeros(n_cols)
-    while (i >= 0):
-        x[i] = b[i] - np.dot(A[i, i + 1:], x[i + 1:])
-        i-=1
-
-    return (x)
 
 def LU_decomposition(A):
 
@@ -111,10 +72,8 @@ def complexity(nmax, every=1, **options):
     for n in range(2, nmax, every):
         times.append(n_test(n))
     
-    import matplotlib.pyplot as plt
-    from scipy.optimize import curve_fit
+
     model = lambda x, exponent, constant: constant * x**exponent
-    plt.style.use('seaborn')
     popt, pcov = curve_fit(model, range(len(times)), times)
     plt.plot(times, label=f'Exponent: {popt[0]:.3f} +- {pcov[0,0]:.3f}')
     plt.plot(range(len(times)), model(range(len(times)), *popt))
