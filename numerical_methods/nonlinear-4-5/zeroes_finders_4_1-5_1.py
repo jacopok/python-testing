867191ee4b57c90c75ccf2886a899227b80d8f22
jacopo <jacopok@gmail.com>
2020-01-15 15:39:52 +0100

    Added comments

--- numerical_methods/nonlinear-4-5/zeroes_finders_4_1-5_1.py
+++ numerical_methods/nonlinear-4-5/zeroes_finders_4_1-5_1.py
@@ -11,25 +11,33 @@ rc('text.latex', preamble=r'''\usepackage{amsmath}
           \usepackage{siunitx}
           ''')
 
-def relaxation(f, x0, eps=1e-6, rel=1.):
+def relaxation(f, x0, eps=1e-6, rel=1., verbose=False):
     """
     Inputs: 
     f: 1-variable function.
         The system should look like x = f(x)
     x0: starting value
     eps: optional, threshold for convergence
-    rel: relaxation parameter (set >1 for extrapolation)
+    rel: relaxation parameter 
+        (set rel>1 for extrapolation, rel<1 for under-relaxation)
+        under relaxation seems to work best
+    verbose: defaults to False, then nothing is printed
+        if it is True, the number of iterations is printed
 
     Finds a zero of f in the interval by iterating the 
     function.
     """    
 
     x = x0
+    n = 0
     while True:
         xold = x
         x = f(x)*rel + xold*(1-rel)
         if (np.abs(x - xold) < eps):
+            if(verbose):
+                print(f'Number of iterations: {n}')
             return (x)
+        n+=1
 
 def bisection(f, interval, eps=1e-10, verbose=False):
     """
@@ -56,10 +64,26 @@ def bisection(f, interval, eps=1e-10, verbose=False):
         else:
             x0 = xmid
         number_iterations += 1
-        if (number_iterations % 100 == 0 and verbose):
-            print(f"{number_iterations} iterations")
+        if(verbose):
+            if (number_iterations % 100 == 0):
+                print(f"{number_iterations} iterations")
 
 def newton_rhapson(f, x0, f_prime=None, eps=1e-10, cdeps=1e-11, verbose=False):
+    """
+    Inputs: 
+    f: 1-variable function
+    x0: initial ansatz from which the search starts
+    f_prime: optional, 1-variable function: the derivative of f 
+        if it is None (or not given) then the derivative is calculated numerically
+
+    eps: optional, threshold for convergence, defaults to 1e-10
+    cdeps: optional, step for the numerical derivative, defaults to 1e-11
+    verbose: optional defaults to False: if it is True the error is printed
+        before returning the value
+
+    Finds a zero of f in the interval.
+    """
+
     if (not f_prime):
         f_prime = lambda x : (f(x+cdeps/2) - f(x-cdeps/2))/cdeps
 
@@ -74,6 +98,17 @@ def newton_rhapson(f, x0, f_prime=None, eps=1e-10, cdeps=1e-11, verbose=False):
             return (x)
 
 def symbolic_newton_rhapson(f, x0, eps=1e-10, verbose=False):
+    """
+    Inputs:
+    f: sympy function
+    x0: initial ansatz
+
+    eps: tolerace, defaults to 1e-10
+
+    Finds a zero of the function using the Newton-Rhapson method, 
+        with a derivative which is calculated symbolically by sympy. 
+    """
+
     x=x0
     x_symb = sp.symbols('x')
 
@@ -103,7 +138,7 @@ if __name__ == "__main__":
     starting_point = 1
     other_extreme = 6
 
-    Es = np.linspace(0, 3*np.pi)
+    Es = np.linspace(0, 2*np.pi)
     colors = iter(plt.get_cmap('Set1').colors)
     for ecc, F in zip(ecc_list, F_list):
         print(f'Eccentricity is ecc={ecc:.2f}, mean anomaly is F={F:.2f}')
