8acd9b82f4127bf14e7f68248cb8689c730acd9f
jacopo <jacopok@gmail.com>
2020-01-15 14:03:10 +0100

    Polished complexity testing
    Former-commit-id: 24cb52893db0397c6fc0ea1f83e9716a8616a38f

--- numerical_methods/linalg-3/complexity_testing.py
+++ numerical_methods/linalg-3/complexity_testing.py
@@ -7,27 +7,28 @@ from scipy.optimize import curve_fit
 from gaussian_elimination_3_1 import gaussian_elimination
 from test_data_generation import test_data
 
-def complexity(nmax, every=1, **options):
+def complexity(sizes, alg=gaussian_elimination, **options):
     from time import time
     from random import sample, randrange
 
     def n_test(n):
         t1 = time()
-        gaussian_elimination(*test_data(n, **options))
+        alg(*test_data(n, **options))
         t2 = time()
         return (t2 - t1)
     
-    sizes = range(2, nmax, every)
-
     times= []
     for n in sizes:
         times.append(n_test(n))
     
-    model = lambda x, exponent, constant: constant * x**exponent
-    popt, pcov = curve_fit(model, range(len(times)), times)
+    model = lambda x, exponent, constant: constant +  x * exponent
+    
+    # want to do a logarithmic fit, but with constant errors on the linear data
+    # so, we do linear error propagation
+    popt, pcov = curve_fit(model, np.log(sizes), np.log(times), sigma=1/np.array(times))
     plt.plot(sizes, times, label=f'Exponent: {popt[0]:.3f} +- {pcov[0,0]:.3f}')
-    plt.plot(sizes, model(sizes, *popt))
+    plt.plot(sizes, np.exp(model(np.log(sizes), *popt)), linestyle=':')
     plt.legend()
-    plt.xlabel('')
-    return (popt[0], pcov[0, 0])
+    plt.xlabel('Matrix dimension')
+    plt.ylabel('Time [s]')
     
