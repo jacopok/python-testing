a30843df992ba87360bd5f2bbf88cdb93c197210
jacopo <jacopok@gmail.com>
2020-01-16 18:30:34 +0100

    Made nice plotter for shooting
    Former-commit-id: ffccfb96c8b3a8cbb3a4c2f98e0e8ab0ce72c9e6

--- numerical_methods/ODE-8/shooting_8_5.py
+++ numerical_methods/ODE-8/shooting_8_5.py
@@ -13,7 +13,7 @@ def cost_function(x_vec, final_pos=10):
 # GRADIENT DESCENT IMPLEMENTATION
 
 def shooting_gd(diffeq, diffeq_params, diffeq_h0, guess,
-    cost_function, eps=1e-3, threshold=1e-5, rate=20., integrator=euler):
+    cost_function, eps=1e-3, threshold=1e-5, rate=20., integrator=euler, plot=False):
 
     x0 = guess
     ts, xs = integrator(diffeq, *diffeq_params, x0, h=diffeq_h0)
@@ -25,9 +25,21 @@ def shooting_gd(diffeq, diffeq_params, diffeq_h0, guess,
 
         grad_cost = cost_function(xs_eps) - cost_function(xs)
 
+        if (plot):
+            lab=None
+            if (iterations % 3 == 0):
+                lab=f"v0 = {x0[1]:.2f}, cost= {cost_function(xs):.2e}"
+            plt.plot(xs[:,0], label=lab)
+
         x0 += np.array([0., - rate * grad_cost])
         iterations += 1
     
+    if (plot):
+        plt.legend()
+        plt.xlabel('$x$ coordinate')
+        plt.ylabel('$y$ coordinate')
+        plt.show()
+
     print(f"Reached convergence in {iterations} iterations.")
     
     return (x0, ts, xs)
@@ -37,4 +49,5 @@ if __name__ == "__main__":
     params=(0., 3.)
     h0=.1
 
-    x0, ts, xs=shooting_gd(grav, params, h0, x0, cost_function)
+    x0, ts, xs = shooting_gd(grav, params, h0, x0, cost_function, plot=True)
+    
