29d4dbd3368c57ca69379e42c01fafcb4a2ef059
jacopo <jacopok@gmail.com>
2020-02-18 18:55:05 +0100

    wrote hangman solver
    Former-commit-id: 14599e1ae24875e0de8a229ed7d35af95eb00da9

---
+++ old_projects/hangman/hangman.py
@@ -0,0 +1,44 @@
+words = []
+
+with open('copy.txt') as file:
+  for line in file:
+    word = line.split('/')[0]
+    words.append(word)
+
+vowels = ['a', 'e', 'i', 'o', 'u']
+
+shapes = {}
+
+
+for word in words:
+  shape = ''
+  for letter in word:
+    if letter in vowels:
+      shape = shape + 'v'
+    else:
+      shape = shape + 'c'
+  
+  if shape in shapes:
+    shapes[shape].append(word)
+  else:
+    shapes[shape] = [word]
+
+def solve(string):
+  initial = string[0]
+  final = string[-1]
+  vcs = string[1:-1]
+  if initial in vowels:
+    vcs = 'v' + vcs
+  else:
+    vcs = 'c' + vcs
+  if final in vowels:
+    vcs = vcs + 'v'
+  else:
+    vcs = vcs + 'c'
+
+  words = shapes[vcs]
+  sol=[]
+  for w in words:
+    if (w[0] == initial and w[-1] == final):
+      sol.append(w)
+  return(sol)
