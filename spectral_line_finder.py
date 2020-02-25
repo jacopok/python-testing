e4c0cce95ef35ea3882ef3c623a77b68e052c81e
jacopo <jacopok@gmail.com>
2020-02-25 22:20:35 +0100

    Created spectral line finder

---
+++ spectral_line_finder.py
@@ -0,0 +1,88 @@
+from astroquery.nist import Nist
+import scipy.constants as sc
+from astropy.constants import codata2018 as ac
+from astropy.constants import iau2015 as aa
+import astropy.units as u
+from astropy.cosmology import Planck15 as cosmo
+import astropy.uncertainty as aun
+import re
+import numpy as np
+from tqdm import tqdm
+
+# return all digits, ignore everything else
+def dig(arr):
+  return_arr = []
+  for x in arr:
+    try:
+      string = re.sub('\D', '', x)
+    except (TypeError):
+      return_arr.append(0.)
+      continue
+    try:
+      return_arr.append(float(string))
+    except (ValueError):
+      return_arr.append(0.)
+  return(return_arr)
+
+def find_candidate_ions(wavelengths, errors):
+  ranges = []
+  if errors is None:
+    errors = wavelengths / 200
+  for w, e in zip(wavelengths, errors):
+    ranges.append((w - e, w + e))
+  
+  tables = []
+  for r in ranges:
+    tables.append(Nist.query(*r, linename="All spectra"))
+
+  sets = []
+  for t in tables:
+    ions = t['Spectrum']
+    sets.append(set(ions))
+  
+  common = list(set.intersection(*sets))
+  return (common)
+
+def ion_goodness(ion, wls, N, verbose=False):
+  delta = max(wls) - min(wls)
+  range = (min(wls) - delta / 5, max(wls) + delta / 5)
+
+  table = Nist.query(*range, linename=ion)
+  table['Rel.'] = dig(table['Rel.'])
+  max_int = max(table['Rel.'])
+  table.sort('Rel.', reverse=True)
+  first_N_wls = list(table[:N]['Observed'])
+
+  dist=0
+  for w in wls.value:
+    pos = np.argmin(np.abs(first_N_wls - w))
+    dist += np.abs(first_N_wls - w)[pos]
+    if (verbose):
+      print(f'For wavelength {w} found line at:')
+      print(first_N_wls[pos])
+      print(f'with intensity {100 * table[pos]["Rel."] / max_int}% of the max')
+      print()
+    first_N_wls.pop(pos)
+  
+  return (dist)
+
+def find_ion(wavelengths, errors=None, N=None):
+  if N is None:
+    N = len(wavelengths) * 3
+  print('Looking for candidate ions')
+  candidate_ions = find_candidate_ions(wavelengths, errors)
+  print(f'Found {len(candidate_ions)} candidates')
+
+  print('Checking goodness')
+  goodnesses = {}
+  for ion in tqdm(candidate_ions):
+    goodnesses[ion] = ion_goodness(ion, wavelengths, N)
+  
+  # sort dict
+  goodnesses = {k: v for k, v in sorted(goodnesses.items(), key=lambda item: item[1])}
+
+  i = next(iter(goodnesses))
+  print(f'Best ion: {i}')
+  ion_goodness(i, wavelengths, N, verbose=True)
+
+  return(goodnesses)
