a8f740485fb30d2e7982994d7212a92b8c72a74e
jacopo <jacopok@gmail.com>
2020-04-02 11:33:16 +0200

    Started conforming to PEP
    Former-commit-id: 6dc4bc096b3f3b562800b52d8ca0c50eb8d05612

--- quantum_optics/analysis.py
+++ quantum_optics/analysis.py
@@ -1,17 +1,17 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from astropy.visualization import astropy_mpl_style
-plt.style.use(astropy_mpl_style)
-from astropy import units as u
-from tqdm import tqdm
-import pandas as pd
 from collections import defaultdict
 from multiprocessing import Pool
 from functools import partial
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.visualization import astropy_mpl_style
+from astropy import units as u
+import pandas as pd
 import numba
 from matplotlib import cm
-from scipy.special import factorial
 from scipy.stats import poisson
+
+plt.style.use(astropy_mpl_style)
 
 THERMAL_PATH = 'data/24, Jan, 2020 - Thermal/'
 COHERENT_PATH = 'data/24, Jan, 2020 - Coherent/'
@@ -22,69 +22,101 @@ THERMAL_NAMES = [THERMAL_PATH + 'Part_' + str(i) + '.txt' for i in range(10)]
 COHERENT_NAMES = [COHERENT_PATH + 'Part_' + str(i) + '.txt' for i in range(10)]
 
 def read_file(name):
+    """Returns a pandas dataframe from the comma-separated file at name"""
+
     data = pd.read_csv(name, sep=',', header=None, names=['ticks', 'channel'], dtype=np.int)
-    return (data)
+    return data
 
 def get_ticks(name):
+    """Returns the array of times contained in the file at name"""
     data = read_file(name)
     ticks = data['ticks'].values
-    return(ticks)
+    return ticks
 
 def get_ticks_names(names):
-    all_ticks=[]
+    """Returns a list of arrays of times from a list of names.
+    All of these are normalized so that the first time is zero.
+    """
+
+    all_ticks = []
     for name in names:
         all_ticks.append(get_ticks(name))
-    return ([x - x[0] for x in all_ticks])
+    return [x - x[0] for x in all_ticks]
 
-thermal_ticks = get_ticks_names(THERMAL_NAMES)
-coherent_ticks = get_ticks_names(COHERENT_NAMES)
+THERMAL_TICKS = get_ticks_names(THERMAL_NAMES)
+COHERENT_TICKS = get_ticks_names(COHERENT_NAMES)
 
 def sum_arrays(arrays):
+    """Given a list of arrays, returns an array as long as the
+    longest of these, containing the sum of all the arrays
+    at corresponding indices.
+    """
+
     max_len = max([len(a) for a in arrays])
     result = np.zeros(max_len)
     for a in arrays:
         result[:len(a)] += a
-    return(result)
+    return result
 
 @numba.jit(nopython=True)
-def base_get_n_in_window(ticks, adim_window):
+def _base_get_n_in_window(ticks, adim_window):
+    """Helper function - returns the number of events observed in a specific window size
+    for a single array.
+
+    Arguments:
+    ticks -- an array of time measurement.
+    adim_window -- window size in the same units as the time measurements.
+
+    Returns:
+    counts -- an array whose i-th element is the number of windows of size
+        adim_window in which i events were found.
+    """
     tmax = ticks[-1]
-    N = int(tmax / adim_window)
-    nums = np.zeros(N)
+    number_of_windows = int(tmax / adim_window)
+    nums = np.zeros(number_of_windows)
 
-    indices = np.searchsorted(ticks, np.arange(N+1)*adim_window)
+    indices = np.searchsorted(ticks, np.arange(number_of_windows+1)*adim_window)
     nums = np.ediff1d(indices)
-    n = np.zeros(max(nums)+1)
+    counts = np.zeros(max(nums)+1)
     for num in nums:
-        n[num] += 1
-    return (n)
+        counts[num] += 1
+    return counts
 
 
-def get_n_in_window_from_ticks(ticks, window, resolution = RESOLUTION):
-        """
-    divide the ticks (which must start at zero!)
-    into "window" long intervals
-    and returns an array of integers, 
-    each entry of which is the number of photons detected
-    in that window
+def get_n_in_window_from_ticks(ticks, window, resolution=RESOLUTION):
+    """Returns the number of events observed in a specific window size
+    for a single array.
+    May use a lot of memory for small window sizes (less than 10ns).
+
+    Arguments:
+    ticks -- an array of time measurement.
+    window -- window size - must be an astropy quantity which can be converted to
+        a time measurement.
+
+    Keyword arguments:
+    resolution -- conversion factor between the integer ticks and physical times,
+        one tick should correspond to a time equal to this constant.
+        Must be an astropy quantity which can be converted to a time measurement.
+        Defaults to the global variable RESOLUTION.
+
+    Returns:
+    counts -- an array whose i-th element is the number of windows of size
+        adim_window in which i events were found.
     """
 
     len_ticks = len(ticks)
-    # print(len_ticks)
-    additional_factor_small_windows = int(max( (np.sqrt(1*u.us / window)).to(u.dimensionless_unscaled).value, 1))
-    # total_ticks = int(MAX_TICKS * additional_factor_small_windows)
     total_ticks = MAX_TICKS
 
     number_subdivisions = len_ticks // total_ticks
     adim_window = (window/resolution).to(u.dimensionless_unscaled).value
 
     all_nums = []
-    for n in range(number_subdivisions):
-        current_ticks = ticks[n * total_ticks:(n + 1) * total_ticks]
-        nums = base_get_n_in_window(current_ticks - current_ticks[0], adim_window)
+    for subdivision in range(number_subdivisions):
+        current_ticks = ticks[subdivision * total_ticks:(subdivision + 1) * total_ticks]
+        nums = _base_get_n_in_window(current_ticks - current_ticks[0], adim_window)
         all_nums.append(nums)
 
-    return(sum_arrays(all_nums))
+    return sum_arrays(all_nums)
 
 def get_n_in_window_from_all_ticks(all_ticks, window):
 
@@ -99,9 +131,9 @@ def get_n_in_window_from_all_ticks(all_ticks, window):
     pool.close() # no more tasks
     pool.join()    # wrap up current tasks
 
-    return (sum_arrays(all_ns))
+    return sum_arrays(all_ns)
     
-def get_photon_counts(thermal_ticks, coherent_ticks, window):
+def get_photon_counts(window, thermal_ticks=THERMAL_TICKS, coherent_ticks=COHERENT_TICKS):
     photon_counts = {}
     photon_counts['thermal'] = get_n_in_window_from_all_ticks(thermal_ticks, window)
     photon_counts['coherent'] = get_n_in_window_from_all_ticks(coherent_ticks, window)
@@ -143,9 +175,11 @@ def describe(distribution, dist_type):
 
     description['variance'] = moment(nums, distribution, 2)
     description['theoretical variance'] = th_desc['variance']
-    description['skewness'] = moment(nums, distribution, 3) / moment(nums, distribution, 2)**(3/2)
+    description['skewness'] = (moment(nums, distribution, 3)
+                               / moment(nums, distribution, 2)**(3/2))
     description['theoretical skewness'] = th_desc['skewness']
-    description['kurtosis'] = moment(nums, distribution, 4) / moment(nums, distribution, 2)**(4/2)
+    description['kurtosis'] = (moment(nums, distribution, 4)
+                               / moment(nums, distribution, 2)**(4/2))
     description['theoretical kurtosis'] = th_desc['kurtosis']
 
     return (description)
@@ -158,7 +192,11 @@ def plot_descriptions(windows, descriptions):
         for (characteristic, color) in zip(description[0], colors):
             ls = '--' if 'theoretical' in characteristic else '-'
 
-            axs[i].loglog(windows, [y[characteristic] for y in description], label=characteristic, ls=ls, c=color)
+            axs[i].loglog(windows,
+                          [y[characteristic] for y in description],
+                          label=characteristic,
+                          ls=ls,
+                          c=color)
 
         axs[i].set_title(name)
         axs[i].legend()
@@ -171,7 +209,7 @@ def get_descriptions(windows):
     descriptions = defaultdict(list)
 
     for window in windows:
-        photon_counts = get_photon_counts(thermal_ticks, coherent_ticks, window)
+        photon_counts = get_photon_counts(window)
 
         for name, distribution in photon_counts.items():
             description = describe(distribution, name)
@@ -197,7 +235,7 @@ def get_rate(descriptions, windows, unit = u.kHz):
 if __name__ == '__main__':
     windows = np.logspace(-2, 3.5, num=40) * u.us
 
-    # photon_counts = get_photon_counts(thermal_ticks, coherent_ticks, 10*u.us)
+    # photon_counts = get_photon_counts(10*u.us)
     
     # for name, distribution in photon_counts.items():
     #     plt.bar(range(len(distribution)), distribution, label=name, alpha=.5)     
