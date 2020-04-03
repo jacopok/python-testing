bf356b7ce7695d0ecc5466cdeb65ae93b0375c46
jacopo <jacopok@gmail.com>
2020-04-03 15:53:55 +0200

    Created interactive distributions plotter

--- quantum_optics/single_photon/bayesian_hyp_testing.py
+++ quantum_optics/single_photon/bayesian_hyp_testing.py
@@ -2,9 +2,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import numba
 
-from simulation import simulate_detections
 from simulation import g_from_detections
 from simulation import detections
+from simulation import simulate_detections_classical
+from simulation import simulate_detections_quantum
 
 SAMPLE_SIZE = int(1e5)
 
@@ -22,21 +23,32 @@ def box_muller(uniform, std):
 
 
 @numba.njit(parallel=True)
-def _generate_detections_classical(error_rate, probability_rate, gate_count):
+def _generate_detections_classical(error_rate, rate, gate_count):
+    # DOES NOT REPRODUCE CORRECT VARIANCES
 
-    error_rate_1 = gate_count * error_rate
-    error_rate_12 = gate_count * error_rate * probability_rate
+    error_rate_1 = np.abs(error_rate - 2 * error_rate * rate)
+    error_rate_12a = (error_rate**2 * (1 - rate)**2)
+    error_rate_12b = (2 * error_rate * (1 - error_rate) * rate * (1 - rate))
+    error_rate_12c = ((2 * error_rate - error_rate**2) * rate**2)
+    error_rate_12 = np.abs(error_rate_12a + error_rate_12b - error_rate_12c)
+    print(error_rate_12)
 
-    error_1 = box_muller(np.random.random(size=SAMPLE_SIZE), error_rate_1)
-    error_2 = box_muller(np.random.random(size=SAMPLE_SIZE), error_rate_1)
-    error_12 = 0 * np.abs(
-        box_muller(np.random.random(size=SAMPLE_SIZE), error_rate_12))
-    distribution_1 = (np.random.binomial(
-        n=gate_count, p=probability_rate, size=SAMPLE_SIZE) + error_1)
-    distribution_2 = (np.random.binomial(
-        n=gate_count, p=probability_rate, size=SAMPLE_SIZE) + error_2)
-    distribution_12 = (np.random.binomial(
-        n=gate_count, p=probability_rate**2, size=SAMPLE_SIZE) + error_12) + error_rate_12
+    error_1 = np.random.binomial(
+        n=gate_count, p=error_rate_1, size=SAMPLE_SIZE) * np.sign(1 - 2 * rate)
+    error_2 = np.random.binomial(
+        n=gate_count, p=error_rate_1, size=SAMPLE_SIZE) * np.sign(1 - 2 * rate)
+    error_12 = np.random.binomial(
+        n=gate_count, p=error_rate_12,
+        size=SAMPLE_SIZE) * np.sign(error_rate_12a + error_rate_12b -
+                                    error_rate_12c)
+
+    distribution_1 = (
+        np.random.binomial(n=gate_count, p=rate, size=SAMPLE_SIZE) + error_1)
+    distribution_2 = (
+        np.random.binomial(n=gate_count, p=rate, size=SAMPLE_SIZE) + error_2)
+    distribution_12 = (
+        np.random.binomial(n=gate_count, p=rate**2, size=SAMPLE_SIZE) +
+        error_12)
 
     return (distribution_1, distribution_2, distribution_12, gate_count)
 
@@ -110,17 +122,27 @@ def plot_distribution_quantum(*args):
 
 
 def plot_both_distributions(*args):
-    g_q = generate_distribution_quantum(*args)
-    g_c = generate_distribution_classical(*args)
-    bins = np.linspace(0, 2, num=50)
-    error_rate, probability_rate, gate_count = args
+    g_q = g_from_detections(*simulate_detections_quantum(*args))
+    g_c = g_from_detections(*simulate_detections_classical(*args))
+    bins = np.linspace(0, 2, num=200)
+    sample_size, probability_rate, error_rate = args
     plt.hist(g_q, bins=bins, density=True, alpha=.5, label='Quantum')
     plt.hist(g_c, bins=bins, density=True, alpha=.5, label='Classical')
 
     print(f'Quantum std: {np.std(g_q)}')
     print(f'Classical std: {np.std(g_c)}')
-    plt.title(f'Pdfs with error {error_rate}, rate {probability_rate},'
-              f' number of particles in gate {gate_count}')
+    plt.title(f'Pdfs with error {error_rate}, rate {probability_rate}'
+              f', sample size {sample_size}')
     plt.xlabel('$g^{(2)}$')
     plt.legend()
     plt.show(block=False)
+
+
+def check_simulation(e_rate, rate, N_gate):
+    x = simulate_detections(SAMPLE_SIZE, rate, rate, e_rate, e_rate,
+                            int(np.sqrt(N_gate)), int(np.sqrt(N_gate)))
+    y = generate_detections_classical(e_rate, rate, N_gate)
+    plt.hist(x.N_12, label='sim', alpha=.5, density=True)
+    plt.hist(y.N_12, label='gen', alpha=.5, density=True)
+    plt.legend()
+    plt.show()
