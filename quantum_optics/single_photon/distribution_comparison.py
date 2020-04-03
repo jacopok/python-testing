146c0dd8c73854a1bbb14f78bb3b23a9a98f5cfa
jacopo <jacopok@gmail.com>
2020-04-03 15:57:29 +0200

    Improved the interactive thingie a bit, removed old file

--- quantum_optics/single_photon/distribution_comparison.py
+++
@@ -1,7 +0,0 @@
-%run bayesian_hyp_testing.py 
-x = simulate_detections(10000, .1, .1, .01, .01) 
-y = generate_detections_classical(.01, .1, 100_000) 
-plt.hist(x.N_12, label='sim', alpha=.5, density=True) 
-plt.hist(y.N_12, label='gen', alpha=.5, density=True) 
-plt.legend() 
-plt.show()                                                                                 
