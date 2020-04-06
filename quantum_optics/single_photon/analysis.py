c102a56bba1a6556672e7c013660584d908aa174
jacopo <jacopok@gmail.com>
2020-04-06 15:09:30 +0200

    Changed the priors around
    Former-commit-id: 85ccc6d6f9aab835fe9bfd8c55bb92d0965d647f

--- quantum_optics/single_photon/analysis.py
+++ quantum_optics/single_photon/analysis.py
@@ -15,3 +15,14 @@ def read_file(name):
                        comment='#',
                        dtype=np.int)
     return data
+
+
+def get_ticks(name):
+    """Returns the arrays of times contained in the file at name"""
+
+    data = read_file(name)
+    
+    ticks_t = data[data['channel'] == 2]['ticks'].values
+    ticks_r = data[data['channel'] == 3]['ticks'].values
+    ticks_g = data[data['channel'] == 4]['ticks'].values
+    return (ticks_t, ticks_r, ticks_g)
--- quantum_optics/single_photon/parameter_estimates.py
+++ quantum_optics/single_photon/parameter_estimates.py
@@ -1,8 +1,10 @@
 import numpy as np
 import astropy.units as u
-from bayesian_hyp_testing import loguniform_dist
+from bayesian_hyp_testing import loguniform_dist, normal_dist
+import uncertainties as un
+# import uncertainties.umath as um
 
-RESOLUTION = 81 * u.picosecond
+RESOLUTION = 80.955 * u.picosecond
 
 GATE_FREQ = 34.95490663 * u.kHz
 
@@ -14,19 +16,30 @@ OBS_TIME = 1 * u.min
 
 WINDOW_COINCIDENCE = 30 * 2 * RESOLUTION
 
-N_G = 1_554_341
+
+def pv(N):
+    """Poisson error on a variable"""
+    return un.ufloat(N, np.sqrt(N))
+
+
+N_G = pv(1_554_341)
 N_1 = 1_056_698
 N_2 = 1_000_384
-N_G1 = 13_930
-N_G2 = 14_829
+N_G1 = pv(13_930)
+N_G2 = pv(14_829)
 N_G12 = 3
 
-g = N_G * N_G12 / (N_G1 * N_G2)
+g = (N_G * N_G12 / (N_G1 * N_G2)).n
 
 N_NOISE = (WINDOW_COINCIDENCE * DETECTOR_FREQ).to(1).value
 RATE = (N_G1 + N_G2) / (2 * N_G)
 E_RATE = N_NOISE / N_G
 
-e_rate, e_rate_pdf = loguniform_dist(-10, 10, num=50)
-rate = [RATE]
-param_prior = np.outer([1], e_rate_pdf)
+e_rate, e_rate_pdf = loguniform_dist(-8, 8, num=25)
+rate, rate_pdf = normal_dist(RATE.n, 50 * RATE.s, num=10)
+# rate, rate_pdf = loguniform_dist(-4, 2, num=50)
+
+
+param_prior = np.outer(rate_pdf, e_rate_pdf)
+
+# param_prior_visible = np.outer(rate_pdf / np.gradient(rate), e_rate_pdf / np.gradient(e_rate))
